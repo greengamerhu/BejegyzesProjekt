7ec065f58c306942c9e5d876f509ae99223bfaee
Rimoczi Daniel <rimoczidani007@gmail.com>
2022-09-30 14:24:48 +0200

    UML Diagramm alapján kész vannak a constructokr, a set, get -erek. + a b feladatban lévő apróságok elkészítve

--- src/Bejegyzes.java
+++ src/Bejegyzes.java
@@ -1,2 +1,44 @@
+import java.time.LocalDateTime;
+
 public class Bejegyzes {
+    String szerzo;
+    String tartalom;
+    int likeok;
+    LocalDateTime letrejott;
+    LocalDateTime szerkesztve;
+    public Bejegyzes(String szerzo, String tartalom) {
+        this.szerzo = szerzo;
+        this.tartalom = tartalom;
+        this.letrejott = LocalDateTime.now();
+        this.szerkesztve = LocalDateTime.now();
+    }
+
+    public String getSzerzo() {
+        return szerzo;
+    }
+
+    public String getTartalom() {
+        return tartalom;
+    }
+
+    public int getLikeok() {
+        return likeok;
+    }
+
+    public LocalDateTime getLetrejott() {
+        return letrejott;
+    }
+
+    public LocalDateTime getSzerkesztve() {
+        this.szerkesztve = LocalDateTime.now();
+        return szerkesztve;
+    }
+
+    public void setTartalom(String tartalom) {
+        this.tartalom = tartalom;
+    }
+    public void like() {
+        this.likeok++;
+    }
+
 }
