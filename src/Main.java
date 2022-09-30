d6bf661633502cc5a32928b2a23dc3c1ece8b7af
Rimoczi Daniel <rimoczidani007@gmail.com>
2022-09-30 14:49:38 +0200

    lista letrehozva, benne két bejegyzéssel

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static List<Bejegyzes>  bejegyzesLista = new ArrayList<>();
     public static void main(String[] args) {
         Bejegyzes b1 = new Bejegyzes("Jőzsef", "loooooooooooooooool");
         System.out.println(b1);
+        Bejegyzes b2 = new Bejegyzes("Sanyika", "Esik az eső :(((");
+        bejegyzesLista.add(b1);
+        bejegyzesLista.add(b2);
     }
 }
